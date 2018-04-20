96e645dbd6c2901b18ada81ffff5a05f3c63d34f
katanmichael <michaelkatan@gmail.com>
2018-04-20 18:11:53 +0300

    Profile fragment Added

--- app/src/main/java/com/game/eddieandmichael/activities/MainActivity.java
+++ app/src/main/java/com/game/eddieandmichael/activities/MainActivity.java
@@ -3,6 +3,8 @@ package com.game.eddieandmichael.activities;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
@@ -11,20 +13,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar; //import this
 import android.view.MenuItem;
-import android.widget.Toast;
 
-import com.game.eddieandmichael.fragments.FragmentDogs;
-import com.game.eddieandmichael.fragments.FragmentWalkers;
 import com.game.eddieandmichael.doggiewalker.R;
-import com.game.eddieandmichael.adapters.ViewPagerAdapter;
+import com.game.eddieandmichael.fragments.*;
+import com.game.eddieandmichael.adapters.*;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+
 
     private TabLayout tabLayout;
     private ViewPager viewPager;
     private ViewPagerAdapter adapter;
     DrawerLayout drawerLayout;
 
+    FragmentManager fragmentManager;
+    FragmentTransaction fragmentTransaction;
 
 
     @Override
@@ -40,10 +44,10 @@ public class MainActivity extends AppCompatActivity {
         actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
 
 
+        fragmentManager = getSupportFragmentManager();
 
 
         drawerLayout=findViewById(R.id.drawer_layout);
-
 
         tabLayout=findViewById(R.id.tablayout_id);
         viewPager=findViewById(R.id.viewpager_id);
@@ -64,20 +68,26 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-
-
-
-
-
-
-
         NavigationView navigationView = findViewById(R.id.navigation_view);
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            public boolean onNavigationItemSelected(@NonNull MenuItem item)
+            {
                 item.setChecked(false); // change color if selceted.
-                drawerLayout.closeDrawers();
-                Toast.makeText(MainActivity.this, item.getTitle(), Toast.LENGTH_SHORT).show();
+
+                switch (item.getItemId())
+                {
+                    case R.id.navi_profile:
+                    {
+                        fragmentTransaction = fragmentManager.beginTransaction();
+                        fragmentTransaction.replace(R.id.main_fragmentPlace,new ProfileFragment(),"ProfileScreen");
+                        fragmentTransaction.commit();
+
+                        return true;
+                    }
+
+                }
+
                 return false;
             }
         });
---
+++ app/src/main/java/com/game/eddieandmichael/fragments/ProfileFragment.java
@@ -0,0 +1,48 @@
+package com.game.eddieandmichael.fragments;
+
+import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.game.eddieandmichael.doggiewalker.R;
+import com.game.eddieandmichael.doggiewalker.R;
+
+public class ProfileFragment extends Fragment
+{
+    View thisView;
+    CollapsingToolbarLayout collapsingToolbar;
+
+    ImageView profile_image;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,ViewGroup container,Bundle savedInstanceState)
+    {
+
+        thisView = inflater.inflate(R.layout.profile_fragment,container,false);
+
+        collapsingToolbar = thisView.findViewById(R.id.fragment_profile_collapsingToolBar);
+
+        collapsingToolbar.setExpandedTitleColor(getResources().getColor(android.R.color.transparent));
+
+        profile_image = thisView.findViewById(R.id.profile_frag_imageProfile);
+
+        //Testing for setting up the profile Image
+        profile_image.setImageDrawable(getResources().getDrawable(R.drawable.ic_person_black_24dp));
+
+        return thisView;
+    }
+
+
+}
