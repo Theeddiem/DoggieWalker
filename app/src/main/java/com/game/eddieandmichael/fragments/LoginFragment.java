6c82502d3f81dc0dc034e3f6e27d384270cc95b1
katanmichael <michaelkatan@gmail.com>
2018-04-21 14:36:33 +0300

    Signup fragment ui added

--- app/src/main/java/com/game/eddieandmichael/fragments/LoginFragment.java
+++ app/src/main/java/com/game/eddieandmichael/fragments/LoginFragment.java
@@ -4,14 +4,18 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.game.eddieandmichael.doggiewalker.R;
 
 public class LoginFragment extends Fragment
 {
+
+    TextView signUp_tv;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -25,6 +29,19 @@ public class LoginFragment extends Fragment
         View view;
         view = inflater.inflate(R.layout.login_fragment,container,false);
 
+        signUp_tv = view.findViewById(R.id.frag_signUpText);
+
+        signUp_tv.setOnClickListener(new View.OnClickListener()
+        {
+
+            @Override
+            public void onClick(View view)
+            {
+                FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.main_fragment,new SignupFragment());
+                transaction.commit();
+            }
+        });
 
         return view;
 
---
+++ app/src/main/java/com/game/eddieandmichael/fragments/SignupFragment.java
@@ -0,0 +1,34 @@
+package com.game.eddieandmichael.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.game.eddieandmichael.doggiewalker.R;
+
+public class SignupFragment extends Fragment
+{
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
+    {
+        View view;
+        view = inflater.inflate(R.layout.signup_fragment,container,false);
+
+
+
+        return view;
+    }
+}
