a64073dacd802c1f7e13739bf289363afc8a79b6
katanmichael <michaelkatan@gmail.com>
2018-04-22 08:06:40 +0300

    Google Login WIP

--- app/src/main/java/com/game/eddieandmichael/fragments/SignupFragment.java
+++ app/src/main/java/com/game/eddieandmichael/fragments/SignupFragment.java
@@ -7,12 +7,23 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
 
 import com.game.eddieandmichael.doggiewalker.R;
 
+import org.w3c.dom.Text;
+
 public class SignupFragment extends Fragment
 {
+    Text fullName_et;
+    Text userName_et;
+    Text email_et;
+    Text password_et;
+    Text rePassword_et;
+
+    Button email_btn;
+    Button facebook_btn;
+    Button google_btn;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState)
@@ -27,9 +38,16 @@ public class SignupFragment extends Fragment
     {
         View view;
         view = inflater.inflate(R.layout.signup_fragment,container,false);
-
+        getReferences(view);
 
 
         return view;
+    }
+
+    private void getReferences(View view)
+    {
+        fullName_et = view.findViewById(R.id.signup_fullName_et);
+        userName_et = view.findViewById(R.id.signUp_username_et);
+
     }
 }
