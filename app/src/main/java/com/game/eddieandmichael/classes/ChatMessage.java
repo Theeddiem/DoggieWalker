c0f34c0f6133c3581fde88f686348cef63bb7af5
Theeddiem <33320326+Theeddiem@users.noreply.github.com>
2018-05-18 18:27:27 +0300

    s

--- app/src/main/java/com/game/eddieandmichael/classes/ChatMessage.java
+++ app/src/main/java/com/game/eddieandmichael/classes/ChatMessage.java
@@ -3,59 +3,34 @@ package com.game.eddieandmichael.classes;
 public class ChatMessage {
 
     private String messageText;
-    private String messageUserID;
     private String currentUserID;
+    private String messageUserID;
     private long messageTime;
 
-    public ChatMessage(String messageText, String currentUserID,String messageUserID) {
+
+    public ChatMessage(){//public NO -ARG CONSTRUCTOR NEEDED
+                 }
+
+    public ChatMessage(String messageText, String currentUserID, String messageUserID) {
         this.messageText = messageText;
+        this.currentUserID = currentUserID;
         this.messageUserID = messageUserID;
-        this.currentUserID=currentUserID;
-        // Initialize to current time
         messageTime = System.currentTimeMillis();
-    }
+     }
 
-    public String getMessageUserID() {
-        return messageUserID;
-    }
-
-    public void setMessageUserID(String messageUserID) {
-        this.messageUserID = messageUserID;
+    public String getMessageText() {
+        return messageText;
     }
 
     public String getCurrentUserID() {
         return currentUserID;
     }
 
-    public void setCurrentUserID(String currentUserID) {
-        this.currentUserID = currentUserID;
-    }
-
-    public ChatMessage(){
-
-    }
-
-    public String getMessageText() {
-        return messageText;
-    }
-
-    public void setMessageText(String messageText) {
-        this.messageText = messageText;
-    }
-
-    public String getmessageUserID() {
+    public String getMessageUserID() {
         return messageUserID;
-    }
-
-    public void setmessageUserID(String messageUserID) {
-        this.messageUserID = messageUserID;
     }
 
     public long getMessageTime() {
         return messageTime;
-    }
-
-    public void setMessageTime(long messageTime) {
-        this.messageTime = messageTime;
     }
 }
--- app/src/main/java/com/game/eddieandmichael/fragments/ChatFragment.java
+++ app/src/main/java/com/game/eddieandmichael/fragments/ChatFragment.java
@@ -1,9 +1,7 @@
 package com.game.eddieandmichael.fragments;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,20 +9,17 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.game.eddieandmichael.classes.AllThePosts;
 import com.game.eddieandmichael.classes.ChatMessage;
 import com.game.eddieandmichael.classes.User;
 import com.game.eddieandmichael.doggiewalker.R;
-import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 
 public class ChatFragment extends android.support.v4.app.Fragment implements View.OnClickListener {
@@ -32,31 +27,30 @@ public class ChatFragment extends android.support.v4.app.Fragment implements Vie
     View thisView;
     User currentUser;
 
-    String otherUserID;
-    AllThePosts allThePosts;
-
-
     ImageView sendMessege;
     EditText messegeInput;
+
     ListView listView;
     ArrayList <String> conversation =new ArrayList<>();
     ArrayAdapter<String> listviewAdapater ;
 
+    String OtherUserID;
+    String OtherFullName;
     private static final String TAG = "ChatFragment";
-    private FirebaseFirestore db = FirebaseFirestore.getInstance();
-    private CollectionReference chatsCollectionRef = db.collection("Chats");
-    private DocumentReference newMsgRef = db.collection("Chats").document();
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle bundle)
     {
 
-
-        String OtherUserID = getArguments().getString("UserID");
-        String OtherFullName = getArguments().getString("UserFullName");
-
+        OtherUserID= getArguments().getString("UserID");;
+         OtherFullName = getArguments().getString("UserFullName");
+        Toast.makeText(getActivity(), OtherUserID, Toast.LENGTH_SHORT).show();
         thisView = inflater.inflate(R.layout.chatwindow_fragment,container,false);
+
+
         listviewAdapater = new ArrayAdapter<>( getActivity(), android.R.layout.simple_list_item_1,conversation);;
         getReferences();
         ((AppCompatActivity)getActivity()).getSupportActionBar().setTitle(OtherFullName);
@@ -81,10 +75,17 @@ public class ChatFragment extends android.support.v4.app.Fragment implements Vie
 
 
      public void load(){
+
         String msgInput = messegeInput.getText().toString();
         conversation.add(msgInput);
-         listviewAdapater.notifyDataSetChanged();
+        listviewAdapater.notifyDataSetChanged();
+         db.collection("Chats").document(currentUser.get_ID()+ " " + OtherUserID).
+                 collection(currentUser.getFullName()+ "  with " + OtherFullName).get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+             @Override
+             public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
 
+             }
+         });
     }
 
 
@@ -92,26 +93,11 @@ public class ChatFragment extends android.support.v4.app.Fragment implements Vie
     public void onClick(View v) {
 
         String msgInput = messegeInput.getText().toString();
-        Map<String, Object> messageMap = new HashMap<>();
-        ChatMessage message = new ChatMessage(msgInput,currentUser.get_ID(),"110400685642351129051"); //to do change it to the user your chating with.
-        load();
-        messageMap.put("messageText",message.getMessageText());
-        messageMap.put("currentUserID",message.getCurrentUserID());
-        messageMap.put("messageUserID",message.getMessageUserID());
-        messageMap.put("timeStamp",message.getMessageTime());
-        db.collection("Chats").document()
-                .set(messageMap)
-                .addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void aVoid) { Log.d(TAG, "Msg sent ");     }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {  Log.w(TAG, "Msg failed", e);
-                    }
-                });
+        ChatMessage message = new ChatMessage(msgInput,currentUser.get_ID(),OtherUserID);
 
-    }
+        db.collection("Chats").document(currentUser.get_ID()+ " " + OtherUserID).
+                collection(currentUser.getFullName()+ "  with " + OtherFullName).document().set(message);
+           }
 
 
 }
