2c707cc2d0c37493aa260f799e0a91f8d98ca894
Theeddiem <33320326+Theeddiem@users.noreply.github.com>
2018-05-18 18:51:09 +0300

    chati

--- app/src/main/java/com/game/eddieandmichael/fragments/ChatFragment.java
+++ app/src/main/java/com/game/eddieandmichael/fragments/ChatFragment.java
@@ -1,7 +1,10 @@
 package com.game.eddieandmichael.fragments;
 
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,11 +18,16 @@ import com.game.eddieandmichael.classes.AllThePosts;
 import com.game.eddieandmichael.classes.ChatMessage;
 import com.game.eddieandmichael.classes.User;
 import com.game.eddieandmichael.doggiewalker.R;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
+import java.util.Collection;
 
 
 public class ChatFragment extends android.support.v4.app.Fragment implements View.OnClickListener {
@@ -38,6 +46,8 @@ public class ChatFragment extends android.support.v4.app.Fragment implements Vie
     String OtherFullName;
     private static final String TAG = "ChatFragment";
     FirebaseFirestore db = FirebaseFirestore.getInstance();
+    //CollectionReference ChatsRef = db.collection("Chats");
+
 
 
 
@@ -45,8 +55,8 @@ public class ChatFragment extends android.support.v4.app.Fragment implements Vie
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle bundle)
     {
 
-        OtherUserID= getArguments().getString("UserID");;
-         OtherFullName = getArguments().getString("UserFullName");
+        OtherUserID= getArguments().getString("UserID");
+        OtherFullName = getArguments().getString("UserFullName");
         Toast.makeText(getActivity(), OtherUserID, Toast.LENGTH_SHORT).show();
         thisView = inflater.inflate(R.layout.chatwindow_fragment,container,false);
 
@@ -73,7 +83,6 @@ public class ChatFragment extends android.support.v4.app.Fragment implements Vie
 
     }
 
-
      public void load(){
 
         String msgInput = messegeInput.getText().toString();
@@ -83,7 +92,21 @@ public class ChatFragment extends android.support.v4.app.Fragment implements Vie
                  collection(currentUser.getFullName()+ "  with " + OtherFullName).get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
              @Override
              public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                    String data= "";
+                    for(QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots){
+                        ChatMessage message = documentSnapshot.toObject(ChatMessage.class);
 
+                        String text = message.getMessageText();
+                        String from = message.getCurrentUserID();
+                        String to = message.getMessageUserID();
+
+                        data+=text+" " + "from " + from +"\n" + "to " + to;
+                        conversation.add(text);
+                        listviewAdapater.notifyDataSetChanged();
+
+                    }
+
+                // Toast.makeText(getActivity(),data, Toast.LENGTH_SHORT).show();
              }
          });
     }
@@ -95,9 +118,24 @@ public class ChatFragment extends android.support.v4.app.Fragment implements Vie
         String msgInput = messegeInput.getText().toString();
         ChatMessage message = new ChatMessage(msgInput,currentUser.get_ID(),OtherUserID);
 
+
         db.collection("Chats").document(currentUser.get_ID()+ " " + OtherUserID).
-                collection(currentUser.getFullName()+ "  with " + OtherFullName).document().set(message);
+                collection(currentUser.getFullName()+ "  with " + OtherFullName).add(message)
+        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            @Override
+            public void onSuccess(DocumentReference documentReference) {
+                Log.d(TAG, "onSuccess: WORKS ");
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d(TAG, e.toString());
+            }
+        });
+        load();
            }
+
+
 
 
 }
